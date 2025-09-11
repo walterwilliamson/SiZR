1d370d460b5e2171322ad33cbc4b8ac5998f1b15
Walter Williamson <waltw@umich.edu>
2025-09-11 10:58:56 -0600

    added function documentation and license

--- R/fitting.R
+++ R/fitting.R
@@ -1,3 +1,25 @@
+#' Fit a model to the simulated data
+#'
+#' @param sim_result A list containing the simulated data frame and the coefficient function f_X, returned from make_data()
+#' @param k Number of basis functions to use in the smooth term. Default is 20.
+#' @param method Method for smoothing parameter estimation. Default is "REML".
+#' @param family Family object specifying the error distribution and link function. Default is gaussian().
+#' @param fit Type of fitting function to use. Options are "gam" (default) or "bam".
+#' @param model Type of model to fit. Options are "SiZR" (default) or "mean".
+#' @param data_type Type of data format. Options are "wide" (default) or "long".
+#'
+#' @return A fitted model object.
+#' @export
+#'
+#' @importFrom mgcv gam bam s
+#'
+#' @examples
+#' # Simulate data
+#' sim_data <- make_data(N = 100, J = 5, s2_e = 1, seed = 123)
+#' # Fit SiZR model
+#' fit_SiZR <- fitting(sim_data, model = "SiZR")
+
+
 fitting <- function(sim_result, k = 20, method = "REML", family = gaussian(),
                     fit = "gam", model = "SiZR", data_type = c("wide", "long")){
 
--- R/make_data.R
+++ R/make_data.R
@@ -1,11 +1,32 @@
-make_data <- function(N = integer(), J = integer(), f_X = function(x) x,
-                      s2_e = numeric(), p_X = function(j) runif(j,0,1),
-                      seed = integer()){
+#' Simulate data for SiZR model
+#'
+#' @param N Sample size (number of subjects).
+#' @param J Number of covariates.
+#' @param s2_e Variance of the residual error term.
+#' @param seed Random seed for reproducibility.
+#' @param p_X Function to generate the covariates X. Default is uniform distribution on (0, 1).
+#' @param f_X Coefficient function defining the association between X and Y. Default is the identity function.
+#'
+#' @return A list containing: data frame with simulated data and the coefficient function f_X.
+#' @export
+#'
+#' @examples
+#' # Simulate data with default parameters
+#' sim_data <- make_data(N = 100, J = 5, s2_e = 1, seed = 123)
+#' head(sim_data$df)
+#'
+#' @importFrom stats gaussian rnorm runif
+#'
+
+make_data <- function(N = integer(), J = integer(), s2_e = numeric(), seed = integer(),
+                      p_X = function(j) runif(j, 0, 1), f_X = function(x) x){
   # Set seed
   set.seed(seed)
 
-  #Simulate the data
+  # Simulate the data
+  # Features
   X <- t(vapply(1:N, function(x,j) p_X(j), j=J, numeric(J)))
+  # Outcomes
   Y <- apply(X, 1, function(x, f, s2) mean(f(x)) + rnorm(1, 0, sd=sqrt(s2)),
              f=f_X, s2=s2_e)
 
